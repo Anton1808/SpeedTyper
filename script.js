be0d2c839207f4dce09e2cc28780eca80c499d40
Anton1808 <SkakunAnton1808@gmail.com>
2020-03-24 19:42:04 +0200

    a

---
+++ script.js
@@ -0,0 +1,89 @@
+const word = document.getElementById("word") ;
+const text = document.getElementById("text") ;
+const scoreEl = document.getElementById("score") ;
+const timeEl = document.getElementById("time") ;
+const endgameEl = document.getElementById("end-game") ; 
+const settingsForm = document.getElementById("settings-form") ;
+const difficultySelect = document.getElementById("difficulty") ;
+
+const words = [
+    "apple" , 
+    "home" ,
+    "earth" , 
+    "difficult" , 
+    "element" , 
+    "transparent" ,
+    "inclusion" , 
+    "cat" , 
+    "parrot" , 
+    "coding" , 
+    "robot" , 
+    "programm"
+
+] ;
+
+let randomWord; 
+let score = 0 ;
+let time = 10 ;
+let difficulty = localStorage.getItem("difficulty") !== null ? localStorage.getItem("difficulty") : medium ;
+difficultySelect.value = localStorage.getItem("difficulty") !== null ? localStorage.getItem("difficulty") : medium ;
+text.focus() ;
+
+const timeInterval = setInterval(updateTime, 1000) ;
+
+function updateTime(){
+    time--;
+    timeEl.innerHTML = time + "s" ;
+    if(time === 0) {
+        clearInterval(timeInterval)
+        gameOver() ;
+    }
+}
+
+function gameOver(){
+    endgameEl.innerHTML = `<h1>Время закончилось</h1>
+    <p>Ваш счет ${score}</p>
+    <button onclick="location.reload()">Обновить</button>` ;
+}
+
+function getRandomWord(a , b) {
+    return words[Math.floor(Math.random() * words.length)] ;
+}
+
+function addWordToDOM() {
+    randomWord = getRandomWord() ;
+    word.innerHTML = randomWord ;
+}
+
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score ;
+}
+
+addWordToDOM() ;
+
+text.addEventListener("input" , e => {
+    const insertedText = e.target.value ;
+    if(insertedText === randomWord) {
+        addWordToDOM() ;
+        updateScore() ;
+        e.target.value = "" ;
+        
+        if(difficulty === "hard"){
+            time += 2 ;
+        }
+        else if(difficulty === "middle"){
+            time += 4 ;
+        } 
+        else{
+            time += 6
+        }
+        updateTime() ;
+    }
+}) ;
+
+settingsForm.addEventListener("change" , e => {
+    difficulty = e.target.value ;
+    console.log(difficulty) ;
+    localStorage.setItem("difficulty" , difficulty)
+})
